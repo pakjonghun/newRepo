d18f76394779685b14fc02c5adfb9a98d9cd9487
정훈박 <fireking5997@gmail.com>
2021-03-08 20:02:20 +0900

    modified

---
+++ server.js
@@ -0,0 +1,15 @@
+const { ApolloServer, gql } = require(`apollo-server`);
+
+const typeDefs = gql`
+  type Query {
+    hellow: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hellow: () => "hellow",
+  },
+};
+const server = new ApolloServer({ resolvers, typeDefs });
+server.listen().then(() => console.log("hi"));
